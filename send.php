80c41bf9bb1c0402ae0772d82351717e81663727
mrdreg0 <drak0on@yandex.ru>
2018-08-30 16:58:31 +0400

    fix send.php

--- send.php
+++ send.php
@@ -1,26 +1,22 @@
 <?php
 //проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['name']) and !isset($_POST['email']) and !isset($_POST['phone']) and !isset($_POST['count'])){
+if(!isset($_POST['name']) and !isset($_POST['email']) and !isset($_POST['phone'])){
     echo "Пожалуйста, заполните все поля формы.";
 } else {
 //показываем форму
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$count = $_POST['count'];
 $name = htmlspecialchars($name);
 $email = htmlspecialchars($email);
 $phone = htmlspecialchars($phone);
-$count = htmlspecialchars($count);
 $name = urldecode($name);
 $email = urldecode($email);
 $phone = urldecode($phone);
-$count = urldecode($count);
 $name = trim($name);
 $email = trim($email);
 $phone = trim($phone);
-$count = trim($count);
-mail("drak0on@yandex.ru", "Заявка с сайта", "Имя: ".$name."\r\nE-mail: ".$email."\r\nТелефон: ".$phone."\r\nКоличество: ".$count ,"From: \r\n".$email);
+mail("drak0on@yandex.ru", "Заявка с сайта", "Имя: ".$name."\r\nE-mail: ".$email."\r\nТелефон: ".$phone ,"From: \r\n".$email);
 }
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -28,7 +24,7 @@ mail("drak0on@yandex.ru", "Заявка с сайта", "Имя: ".$name."\r\nE-
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="refresh" content="3; url=index.html">
-<title>С вами свяжутся</title>
+<title>Спасибо за заявку! Мы Вам перезвоним.</title>
 <meta name="generator">
 <style type="text/css">
 body
